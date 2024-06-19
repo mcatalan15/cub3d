aa285a5275eb2bcbd4b9dac636a634b1e477c8a1
Marc Catalán Sánchez <marc.prat.2001@gmail.com>
2024-06-19 14:05:55 +0200

    wtf

---
+++ src/game/rays.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rays.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcatalan <mcatalan@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 12:34:05 by mcatalan          #+#    #+#             */
+/*   Updated: 2024/06/19 14:05:29 by mcatalan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+jpu-krjpu-kr.f>      9jpu-krjpu-krjpu-kr.f>      6jpu-krjpu-krjpu-kr.f>   
+
+void	one_ray(t_mlx_data *data, t_cube *cube, t_vec raydir)
+{
+	double	x;
+	
+	x = 0;
+}
+
+void	create_rays(t_mlx_data *data, t_cube *cube)
+{
+	int		i;
+	double	camerax;
+	t_vec	raydir;
+
+	i = -1;
+	data->p.map.x = (data->p.pos.x - 100) / BLOCK;
+	data->p.map.y = (data->p.pos.y - 100) / BLOCK;
+	raydir.x = data->p.dir.x;
+	raydir.y = data->p.dir.y;
+	one_ray(data, cube, raydir);
+	/*while (++i < WIDTH)
+	{
+		camerax = 2 * i / (double)WIDTH - 1;
+		raydir.x = data->p.dir.x + data->p.plane.x * camerax;
+		raydir.y = data->p.dir.y + data->p.plane.y * camerax;
+	}*/
+}
