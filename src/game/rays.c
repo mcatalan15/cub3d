8b6cf6996bb095f70afa924ad02a3d1b67632cfa
jpaul-kr <jonpk555@gmail.com>
2024-06-24 20:13:14 +0200

    adding funct to create lines

--- src/game/rays.c
+++ src/game/rays.c
@@ -12,6 +12,38 @@
 
 #include "../../includes/cub3d.h"
 
+void    draw_line(t_mlx_data *data, t_vec st, t_vec end, int color)
+{
+        t_ray   r;
+
+        r.dx = abs(r.x1 - r.x0);
+        r.sx = r.x0 < r.x1 ? 1 : -1;
+        r.dy = -abs(r.y1 - r.y0);
+        r.sy = r.y0 < r.y1 ? 1 : -1;
+        r.err = r.dx + r.dy;
+        r.y0 = (int)st.y;
+        r.x0 = (int)st.x;
+        r.y1 = (int)end.y;
+        r.x1 = (int)end.x;
+        while (1)
+        {
+                //printf("x: %d y: %d\n", x0, y0);
+                my_pixel_put(&data->img, r.y0, r.x0, color);
+                if (r.x0 == r.x1 && r.y0 == r.y1) break;
+                        r.e2 = 2 * r.err;
+                if (r.e2 >= r.dy)
+                {
+                        r.err += r.dy;
+                        r.x0 += r.sx;
+		}
+		if (e2 <= dx)
+		{
+			err += dx;
+			y0 += sy;
+		}
+	}
+}
+			5,1           Top
 void	one_ray(t_mlx_data *data, t_cube *cube, t_vec raydir)
 {
 	double	x;
